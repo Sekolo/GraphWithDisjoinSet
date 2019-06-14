ebb109e7879b1c1b7ca3ef0480e5e7b017d08d22
gaston <reyesgaston_95@yahoo.com.ar>
2019-06-14 14:51:33 -0300

    Reconstruyo Utilidades

--- Actividad15AyC/src/Actividad15/Utilidades.java
+++ Actividad15AyC/src/Actividad15/Utilidades.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Actividad15;
 
 import Cola.Cola;
@@ -57,59 +57,6 @@ public class Utilidades {
 	//O(|V|+|E|)
 
 }
-=======
-package Actividad15;
 
-public class Utilidades {
-	
-	
-	public boolean BFS (Grafo G)
-	{
-		boolean[] visitados;
-		int CantVertices=G.getNodosCount();
-		visitados=new boolean[CantVertices];
-		int conexo=0;
-		boolean Esconexo=true;
-		int i=0;
-		for (i=0; i<CantVertices; i++)
-			visitados[i] = false;
-			
 
-		while (i<CantVertices && Esconexo)
-		{		
-			if (!visitados[i])
-			{ 
-				conexo++;
-				if(conexo==1)
-					RecorridoNivel(G,i);
-				else
-					Esconexo=false;
-				
-			}
-			i++;
-		}
-		return Esconexo;
-		
-	}
 
-	public void RecorridoNivel (Grafo G, int v)
-	{
-		Cola Q;
-		visitado[v]=true;
-		Q.add(v);
-			while (!Q.empty()) 
-			{
-					x = Q.extract();
-					
-					foreach (v[y] adyacente a v[x]) 
-					{
-						if (!visitado[y]) 
-							visitado[y]=true; 
-						Q.add(y);
-					}
-			}
-	}
-	//O(|V|+|E|)
-
-}
->>>>>>> b877c5c2254a2daf7497e0f8593095dffba31491
