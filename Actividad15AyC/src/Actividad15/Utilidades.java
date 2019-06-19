a71771c1aabd996e196da8a314cdc6df3f4245d5
gaston <reyesgaston_95@yahoo.com.ar>
2019-06-19 20:36:30 -0300

    Actualizo Utilidades
    super duper duuu!

--- Actividad15AyC/src/Actividad15/Utilidades.java
+++ Actividad15AyC/src/Actividad15/Utilidades.java
@@ -3,11 +3,8 @@ package Actividad15;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-
-import Cola.ColaImp;
 import DisjoinSet.DisjoinSetImp;
 import DisjoinSet.DisjoinSet;
-import Cola.Cola;
 import Grafo.Arco;
 import Grafo.Vertice;
 import Grafo.Grafo;
@@ -59,22 +56,22 @@ public class Utilidades {
 		// Hasta que la cola sea vacia
 		while (queue.size() != 0) {	
 			Vertice s = queue.poll(); 
-			for(int i = 0; i < G.getVertices().size(); i++) {
-				
-				Vertice n = G.getVertices().get(i); 
-				if (!visitados[n.element()])  {
-	        	// Seteo cada adyacente como visitado 
-				// Agrego a la queue cada vertice ady
-					for (int j = 0; j < n.getAdyacentes().size(); j++) {
-	        		
-						visitados[n.element()] = true; 
-						queue.add(n); 
-						
+					for (int j = 0; j < s.getAdyacentes().size(); j++) {
+						try {
+							//Obtengo el vertice opuesto a s
+							Vertice Vady=G.opposite(s , s.getAdyacentes().get(j));
+							//Si no fue visitado lo visito y lo agrego ala cola
+							if (!visitados[Vady.element()])  {
+								visitados[Vady.element()] = true; 
+								queue.add(Vady); 
+							}
+						}
+						catch(Exception e) {}
 					}
 					
 				}
-			}
-		} 
+			
+		
 			
 	}
 	
@@ -86,10 +83,6 @@ public class Utilidades {
         DisjoinSet ds = new DisjoinSetImp(G.getVerticesCount());
         ArrayList<Arco> amc = new ArrayList<>();
 
-       /* //Inicializo el Heap con todos los arcos.
-        for (int i = 0; i < G.getArcosCount() ; i++) {
-          //  heap.insert(G.getArcos().get(i));
-        }*/
    
         System.out.println("arranca kruskal por dentro");
              
@@ -102,9 +95,7 @@ public class Utilidades {
 		for(int i = 0; i < verts.size() ; i++) {
 			ds.makeSet(i);
 			visitados[i] = false;
-			System.out.println("viste el grafo");
-			
-		}
+			}
 		
 		try {
 			Vertice s =  verts.get(0);
@@ -122,9 +113,7 @@ public class Utilidades {
                 	
 						for (int j = 0; j < n.getAdyacentes().size(); j++) {
 							System.out.println("entre a los arcos");
-							heap.insert(n.getAdyacentes().get(j));
-							//ds.union(n.element(), G.opposite(n, n.getAdyacentes().get(j)).element());
-							
+							heap.insert(n.getAdyacentes().get(j));				
 						}
 						visitados[n.element()] = true; 
 						queue.add(n); 
@@ -142,8 +131,6 @@ public class Utilidades {
         while(index < G.getVerticesCount()-1){
         	//Tomo el arco de menor peso
             Arco edge = heap.popMin();
-            
-            //check if adding this edge creates a cycle
             //Checkeo a que set pertenece cada vertice del arco.
             int v1_set = ds.findSet(edge.getV1().element());
             int v2_set = ds.findSet(edge.getV2().element());
@@ -159,93 +146,6 @@ public class Utilidades {
         
         return amc;
     }
-	
-	
-	
-	
-	int partition(int A[], int low, int high) { 
-		int pivote = A[high]; 
-		int i = (low-1); // elemento mas pequeno 
-		for (int j=low; j<high; j++)  { 
-			// Si es elemento es mas chico o igual a pivote 
-			if (A[j] <= pivote) { 
-				i++; 
-
-				// intercambio A[i] and A[j] 
-				int temp = A[i]; 
-				A[i] = A[j]; 
-				A[j] = temp; 
-			} 
-		} 
-
-		// intercambio  A[i+1] and A[high]  
-		int temp = A[i+1]; 
-		A[i+1] = A[high]; 
-		A[high] = temp; 
-
-		return i+1; 
-	} 
-
-
-	/*
-	A[] --> Arreglo a ordenar, 
-	low --> index inicial, 
-	high --> index final */
-	void sort(int A[], int low, int high) { 
-		if (low < high) { 
-			int pi = partition(A, low, high); 
-			sort(A, low, pi-1); 
-			sort(A, pi+1, high); 
-		} 
-	} 
-
-	
-
-	public void RecorridoBFS (Grafo G)
-	{
-		int i=0;
-		// Genero mi arreglo de visitados
-		setVisitados(G.getVerticesCount());
-		// Seteo todos los visitados como false
-		for (int j=0; j<G.getVerticesCount(); j++)
-			visitados[j] = false;
-		
-		while (i<G.getVerticesCount())
-		{		// Para cada Vertice no visitado lo recorro 
-			if (!visitados[i])
-				RecorridoNivel2(G);
-			i++;
-		}
-		
-	}
-	
-	public void RecorridoNivel2(Grafo G)
-	{
-		LinkedList<Vertice> queue = new LinkedList<Vertice>();
-		// Obtengo el primer vertice
-		Vertice v = G.getVertices().get(0);
-		queue.add(v);
-		visitados[v.element()]=true;
-		// Hasta que la cola sea vacia
-		while (queue.size() != 0) {	
-			Vertice s = queue.poll(); 
-			for(int i = 0; i < G.getVertices().size(); i++) {
-				
-				Vertice n = G.getVertices().get(i); 
-				if (!visitados[n.element()])  {
-	        	// Seteo cada adyacente como visitado 
-				// Agrego a la queue cada vertice ady
-					for (int j = 0; j < n.getAdyacentes().size(); j++) {
-						System.out.print(n.element());
-						visitados[n.element()] = true; 
-						queue.add(n); 
-						
-					}
-					
-				}
-			}
-		} 
-	}
 } 
 
 
